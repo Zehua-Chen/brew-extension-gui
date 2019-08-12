9d574497280fef42d51a889ba61f279722144340
Zehua-Chen <peterchen06242000@outlook.com>
2019-08-13 07:43:56 +0800

    Add Init Files

---
+++ Brew Extension/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Brew Extension
+//
+//  Created by Zehua Chen on 8/13/19.
+//  Copyright © 2019 Zehua Chen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
