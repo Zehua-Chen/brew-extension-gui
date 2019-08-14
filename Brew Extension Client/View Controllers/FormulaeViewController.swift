3846ffeaad6ddcd4b1ce7b9b88fec77856e6ee18
Zehua-Chen <peterchen06242000@outlook.com>
2019-08-14 21:42:31 +0800

    [feat] Populate Dependency Display

--- Brew Extension Client/View Controllers/FormulaeViewController.swift
+++ Brew Extension Client/View Controllers/FormulaeViewController.swift
@@ -11,13 +11,69 @@ import Cocoa
 class FormulaeViewController: NSViewController {
 
     class DependencyDelegateAndDataSource: NSObject, NSTableViewDelegate, NSTableViewDataSource {
-        
+        var brewExt = AppDelegate.shared.brewExtension
+        var outcomings = [String]()
+        var incomings = [String]()
+
+        var formulae = "" {
+            didSet {
+                self.outcomings = [String](self.brewExt.dataBase!.outcomingDependencies(for: self.formulae))
+                self.incomings = [String](self.brewExt.dataBase!.incomingDependencies(for: self.formulae))
+            }
+        }
+
+        func tableView(
+            _ tableView: NSTableView,
+            viewFor tableColumn: NSTableColumn?,
+            row: Int
+        ) -> NSView? {
+            switch tableColumn!.title {
+            case "Depending on":
+                guard row < self.outcomings.count else { return nil }
+
+                let view = tableView.makeView(withIdentifier: .init("dependencyCellView"), owner: nil) as! NSTableCellView
+                view.textField?.stringValue = outcomings[row]
+
+                return view
+            case "Depended by":
+                guard row < self.incomings.count else { return nil }
+
+                let view = tableView.makeView(withIdentifier: .init("dependencyCellView"), owner: nil) as! NSTableCellView
+                view.textField?.stringValue = incomings[row]
+
+                return view
+            default:
+                return nil
+            }
+        }
+
+        func numberOfRows(in tableView: NSTableView) -> Int {
+            if self.outcomings.count > self.incomings.count {
+                return self.outcomings.count
+            }
+
+            return self.incomings.count
+        }
+    }
+
+    class LabelDelegateAndDataSource: NSObject, NSTableViewDelegate, NSTableViewDataSource {
+        var brewExt = AppDelegate.shared.brewExtension
+        var formulae = "" {
+            didSet {
+
+            }
+        }
     }
 
     @IBOutlet weak var formulaeTitleLabel: NSTextField!
-
+    @IBOutlet weak var dependencyTableView: NSTableView!
+    @IBOutlet weak var labelTableView: NSTableView!
+    
     var notificationCenter = NotificationCenter.default
     var formulae: String?
+
+    var dependencyDelegateAndDataSource = DependencyDelegateAndDataSource()
+    var labelDelegateAndDataSource = LabelDelegateAndDataSource()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +84,12 @@ class FormulaeViewController: NSViewController {
             object: nil,
             queue: nil,
             using: self.formulaeClicked)
+
+        self.dependencyTableView.delegate = self.dependencyDelegateAndDataSource
+        self.dependencyTableView.dataSource = self.dependencyDelegateAndDataSource
+
+        self.labelTableView.delegate = self.labelDelegateAndDataSource
+        self.labelTableView.dataSource = self.labelDelegateAndDataSource
     }
 
     func formulaeClicked(_ notification: Notification) {
@@ -35,6 +97,12 @@ class FormulaeViewController: NSViewController {
 
         if self.formulae != nil {
             self.formulaeTitleLabel.stringValue = self.formulae!
+
+            self.dependencyDelegateAndDataSource.formulae = formulae!
+            self.labelDelegateAndDataSource.formulae = formulae!
+
+            self.dependencyTableView.reloadData()
+            self.labelTableView.reloadData()
         }
     }
 }
---
+++ Brew Extension Client/Views/TextCellView.swift
@@ -0,0 +1,9 @@
+//
+//  TextCellView.swift
+//  Brew Extension Client
+//
+//  Created by Zehua Chen on 8/14/19.
+//  Copyright © 2019 Zehua Chen. All rights reserved.
+//
+
+import Cocoa
