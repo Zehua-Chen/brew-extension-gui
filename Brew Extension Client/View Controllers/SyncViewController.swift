0ceb5ebaee39b5aeea3c8b89a56af766efb89149
Zehua-Chen <peterchen06242000@outlook.com>
2019-08-14 19:43:03 +0800

    [feat] Add Sync Sheet

---
+++ Brew Extension Client/View Controllers/SyncViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SyncViewController.swift
+//  Brew Extension Client
+//
+//  Created by Zehua Chen on 8/14/19.
+//  Copyright © 2019 Zehua Chen. All rights reserved.
+//
+
+import Cocoa
+
+class SyncViewController: NSViewController {
+}
