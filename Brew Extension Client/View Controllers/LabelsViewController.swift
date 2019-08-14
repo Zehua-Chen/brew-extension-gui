8391de73202b2774767fa6174852082c362b8799
Zehua-Chen <peterchen06242000@outlook.com>
2019-08-14 11:42:59 +0800

    [feat] Setup Label TableView

---
+++ Brew Extension Client/View Controllers/LabelsViewController.swift
@@ -0,0 +1,62 @@
+//
+//  LabelsViewController.swift
+//  Brew Extension Client
+//
+//  Created by Zehua Chen on 8/14/19.
+//  Copyright © 2019 Zehua Chen. All rights reserved.
+//
+
+import Cocoa
+
+class LabelsViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
+
+    var labels = ["Life", "C++", "Unity"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+
+    // MARK: Event handlers
+    func deleteRowActionClicked(_ action: NSTableViewRowAction, _ rowIndex: Int) {
+    }
+
+    func starRowActionClicked(_ action: NSTableViewRowAction, _ rowIndex: Int) {
+
+    }
+
+    // MARK: NSTableViewDataSource Conformance
+
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return labels.count
+    }
+
+    // MARK: NSTableViewDelegate Conformance
+
+    func tableView(
+        _ tableView: NSTableView,
+        viewFor tableColumn: NSTableColumn?,
+        row: Int
+    ) -> NSView? {
+        let view = tableView.makeView(withIdentifier: .init("labelCellView"), owner: nil) as! LabelCellView
+        view.titleTextField.stringValue = labels[row]
+        view.formulaesCountTextField.stringValue = "2 formulaes"
+
+        return view
+    }
+
+    func tableView(
+        _ tableView: NSTableView,
+        rowActionsForRow row: Int,
+        edge: NSTableView.RowActionEdge
+    ) -> [NSTableViewRowAction] {
+        switch edge {
+        case .leading:
+            return [.init(style: .regular, title: "Star", handler: self.starRowActionClicked)]
+        case .trailing:
+            return [.init(style: .destructive, title: "Delete", handler: self.deleteRowActionClicked)]
+        @unknown default:
+            return []
+        }
+    }
+}
---
+++ Brew Extension Client/Views/LabelCellView.swift
@@ -0,0 +1,14 @@
+//
+//  LabelCellView.swift
+//  Brew Extension Client
+//
+//  Created by Zehua Chen on 8/14/19.
+//  Copyright © 2019 Zehua Chen. All rights reserved.
+//
+
+import Cocoa
+
+class LabelCellView: NSTableCellView {
+    @IBOutlet weak var titleTextField: NSTextField!
+    @IBOutlet weak var formulaesCountTextField: NSTextField!
+}
